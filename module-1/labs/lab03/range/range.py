efbe07bf60cfbacaa3b55937e703391603becfea
Sergey Kabanov <sggplz@yandex.ru>
2018-09-27 20:18:22 +0300

    Update Lab03

--- module-1/labs/lab03/range/range.py
+++ module-1/labs/lab03/range/range.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def main():
-    pass
+
+class RangeIterator:
+
+    def __init__(self, rangeobj):
+        raise NotImplementedError
+
+    def __next__(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise NotImplementedError
+
+
+class Range:
+
+    def __init__(self, *args):
+        raise NotImplementedError
+
+    def __eq__(self, other):
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __contains__(self, value):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    main()
+    import doctest
+    doctest.testmod()
--- module-1/labs/lab03/range/test_range.py
+++ module-1/labs/lab03/range/test_range.py
@@ -8,16 +8,12 @@ except (ModuleNotFoundError, ImportError):
     from range import Range, RangeIterator
 
 
-class Int:
-
-    def __init__(self, value):
-        self.value = int(value)
-
-    def __int__(self):
-        return self.value
-
-
 class TestRange(unittest.TestCase):
+
+    def test_yield(self):
+        with open(Range.__module__ + ".py", 'r') as f:
+            for line in f.readlines():
+                self.assertNotIn("yield", line, "Do not use yield!")
 
     def test_init(self):
         r = Range(2, 100, 5)
@@ -135,77 +131,6 @@ class TestRange(unittest.TestCase):
         self.assertEqual(id(iter(r)), id(iter(iter(r))))
         self.assertRaises(StopIteration, next, iter(r))
 
-    def test_index(self):
-        self.assertEqual(Range(3, 11, 2).index(9), range(3, 11, 2).index(9))
-        self.assertEqual(Range(100).index(98), range(100).index(98))
-        self.assertEqual(
-            Range(5, -1000, -13).index(-216),
-            range(5, -1000, -13).index(-216)
-        )
-        self.assertEqual(
-            Range(19, 13333, 3).index(2713),
-            range(19, 13333, 3).index(2713)
-        )
-        self.assertEqual(
-            Range(19, 13333000, 3).index(37351.0),
-            range(19, 13333000, 3).index(37351.0)
-        )
-        self.assertEqual(
-            Range(101).index(float(100)),
-            range(101).index(float(100))
-        )
-
-        with self.assertRaises(ValueError) as e:
-            Range(3, 11, 2).index(11)
-        with self.assertRaises(ValueError) as exp:
-            range(3, 11, 2).index(11)
-        self.assertEqual(e.exception.args, exp.exception.args)
-
-        with self.assertRaises(ValueError) as e:
-            Range(3, 11, 2).index("a")
-        with self.assertRaises(ValueError) as exp:
-            range(3, 11, 2).index("a")
-        self.assertEqual(e.exception.args, exp.exception.args)
-
-        with self.assertRaises(ValueError) as e:
-            Range(3, 11, 2).index(Int(3))
-        with self.assertRaises(ValueError) as exp:
-            range(3, 11, 2).index(Int(3))
-        self.assertEqual(e.exception.args, exp.exception.args)
-
-    def test_count(self):
-        self.assertEqual(Range(3, 11, 2).count(9), range(3, 11, 2).count(9))
-        self.assertEqual(Range(100).count(98), range(100).count(98))
-        self.assertEqual(
-            Range(5, -1000, -13).count(-216),
-            range(5, -1000, -13).count(-216)
-        )
-        self.assertEqual(
-            Range(19, 13333, 3).count(2713),
-            range(19, 13333, 3).count(2713)
-        )
-        self.assertEqual(
-            Range(19, 13333000, 3).count(37351.0),
-            range(19, 13333000, 3).count(37351.0)
-        )
-        self.assertEqual(
-            Range(101).count(float(100)),
-            range(101).count(float(100))
-        )
-
-        self.assertEqual(
-            Range(101).count("a"),
-            range(101).count("a")
-        )
-        self.assertEqual(
-            Range(101).count([]),
-            range(101).count([])
-        )
-        self.assertEqual(
-            Range(101).count(-17),
-            range(101).count(-17)
-        )
-
     def test_contains(self):
         self.assertEqual(
             17 in Range(0, 18, 1),
@@ -216,12 +141,8 @@ class TestRange(unittest.TestCase):
             101 not in range(101)
         )
         self.assertEqual(
-            "a" not in Range(101),
-            "a" not in range(101)
-        )
-        self.assertEqual(
-            Int(2) not in Range(101),
-            Int(2) not in range(101)
+            1000 not in Range(101),
+            1000 not in range(101)
         )
         self.assertEqual(
             2713 in Range(19, 13333, 3),
@@ -240,44 +161,27 @@ class TestRange(unittest.TestCase):
         self.assertEqual(Range(10)[0], range(10)[0])
         self.assertEqual(Range(10)[4], range(10)[4])
         self.assertEqual(Range(19, 13333, 3)[17], range(19, 13333, 3)[17])
+        self.assertEqual(Range(19, 13333, 3)[0], range(19, 13333, 3)[0])
+        self.assertEqual(Range(19, 13333, 3)[3], range(19, 13333, 3)[3])
         self.assertEqual(Range(19, 13333, 3)[31], range(19, 13333, 3)[31])
         self.assertEqual(Range(-10, -100, -1)[15], range(-10, -100, -1)[15])
         self.assertEqual(Range(-13, -113, -6)[11], range(-13, -113, -6)[11])
+        self.assertEqual(Range(-13, -113, -3)[15], range(-13, -113, -3)[15])
 
-    def test_getitem_slice(self):
-        # self.assertEqual(str(Range(10)[::-1]), str(range(10)[::-1]))
-        # self.assertEqual(str(Range(10)[1:2:3]), str(range(10)[1:2:3]))
-        self.assertEqual(str(Range(10)[1:6:3]), str(range(10)[1:6:3]))
-        self.assertEqual(
-            str(Range(19, 13333, 3)[17:50:3]),
-            str(range(19, 13333, 3)[17:50:3])
-        )
-        self.assertEqual(
-            str(Range(19, 13338, 3)[17:10001:3]),
-            str(range(19, 13338, 3)[17:10001:3])
-        )
-        self.assertEqual(
-            str(Range(19, 13333, 3)[31]), str(range(19, 13333, 3)[31])
-        )
-        # self.assertEqual(
-        #     str(Range(-10, -100, -1)[::-11]), str(range(-10, -100, -1)[::-11])
-        # )
-        self.assertEqual(
-            str(Range(-30, -2)[slice(2, 6, 3)]),
-            str(range(-30, -2)[slice(2, 6, 3)])
-        )
-        self.assertEqual(
-            str(Range(-13, -113, -6)[slice(2, 11, -3)]),
-            str(range(-13, -113, -6)[slice(2, 11, -3)])
-        )
-        self.assertEqual(
-            str(Range(-13, -113, -6)[slice(12, 33, 3)]),
-            str(range(-13, -113, -6)[slice(12, 33, 3)])
-        )
-        # self.assertEqual(
-        #     str(Range(-13, -113, -6)[-3:-10:-2]),
-        #     str(range(-13, -113, -6)[-3:-10:-2])
-        # )
+        with self.assertRaises(IndexError) as e:
+            Range(-13, -113, -6)[133]
+        with self.assertRaises(IndexError) as exp:
+            range(-13, -113, -6)[133]
+        self.assertEqual(e.exception.args, exp.exception.args)
+
+    def test_next(self):
+        r = Range(3)
+        it = iter(r)
+
+        self.assertEqual(next(it), 0)
+        self.assertEqual(next(it), 1)
+        self.assertEqual(next(it), 2)
+        self.assertRaises(StopIteration, next, it)
 
 
 if __name__ == "__main__":
